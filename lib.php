25c3a253ed5709cf23521dee0abfbeae2420c9d0
Petr Skoda <petr.skoda@openlms.net>
2023-03-31 16:19:43 +0200

    OWORK-531 readd udplans

--- lib.php
+++ lib.php
@@ -22,22 +22,3 @@
  * @author     Petr Skoda
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-function local_openlms_extend_navigation(global_navigation $navigation) {
-    if (isloggedin() && !isguestuser()
-        && file_exists(__DIR__ . '/../../admin/tool/udplans/version.php')
-    ) {
-        if (\tool_udplans\local\util::udplans_active()) {
-            if (\tool_udplans\local\plan::get_my_plans()) {
-                $n = $navigation->create(get_string('myplans', 'tool_udplans'),
-                    new moodle_url('/admin/tool/udplans/plans_my.php'),
-                    global_navigation::TYPE_CUSTOM,
-                    null,
-                    'myplans',
-                    new pix_icon('myplans', '', 'tool_udplans'));
-                $n->showinflatnavigation = true;
-                $navigation->add_node($n, 'mycourses');
-            }
-        }
-    }
-}
