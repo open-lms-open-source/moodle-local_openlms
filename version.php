2b76e28a9e4e7a986372fa8f8142f16dd55713ff
Sam Chaffee <sam.chaffee@openlms.net>
2025-06-19 17:10:39 -0600

    OWORK-1558 dev: Prepare 4.5 MP3 RC1

--- version.php
+++ version.php
@@ -31,5 +31,5 @@ $plugin->version   = 2024103100;
 $plugin->requires  = 2024091700.00; // 4.5.0
 $plugin->component = 'local_openlms';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v4.2.0';
+$plugin->release   = 'v4.5-MP3';
 $plugin->supported = [405, 405];
