ef45015c4ac4dea3c4761bfda3bd2d90887e7d81
Sam Chaffee <sam.chaffee@openlms.net>
2025-08-15 14:09:05 -0600

    OWORK-1609 dev: WORK 4.5 MP4 RC1

--- version.php
+++ version.php
@@ -31,5 +31,5 @@ $plugin->version   = 2024103100;
 $plugin->requires  = 2024091700.00; // 4.5.0
 $plugin->component = 'local_openlms';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v4.5-MP3';
+$plugin->release   = 'v4.5-MP4';
 $plugin->supported = [405, 405];
